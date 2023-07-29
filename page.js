5d5075889d38ffdb93fa5a15fdd5490a540a21e1
antonfiluta <antonfiluta2.0@mail.ru>
2023-07-29 17:31:40 +0300

    feat: new ficha, make rates visible

--- page.js
+++ page.js
@@ -1,4 +1,4 @@
-const el = document.querySelector("link");
+const linkS = document.querySelector("link");
 
 let wid = window.innerWidth;
 let hei = window.innerHeight;
@@ -9,9 +9,33 @@ let sizex = wid < hei;
 let i = 0;
 
 if (size) {
-    el.href = "style.css";
+    linkS.href = "style.css";
 } else {
-    el.href = "styleRE.css";
+    linkS.href = "styleRE.css";
+}
+
+let buttonR1 = document.getElementById("rateButton1");
+let buttonR2 = document.getElementById("rateButton2");
+let buttonD = document.getElementById("rateButtonD");
+let el1 = document.getElementById("disEl1");
+let el2 = document.getElementById("disEl2");
+
+function moreRates1() {
+  el1.classList.toggle("display");
+  buttonR1.classList.add("display");
+  buttonR2.classList.toggle("display");
+}
+
+function moreRates2() {
+  el2.classList.toggle("display");
+  buttonR2.classList.add("display");
+  buttonD.classList.toggle("display");
+}
+
+function moreRatesD() {
+  buttonD.classList.add("display");
+  buttonR2.classList.add("display");
+  buttonR1.classList.toggle("display");
 }
 
 

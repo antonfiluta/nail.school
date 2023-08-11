3cc54ad45876a047557b49760ba269bc30439cf3
antonfiluta <antonfiluta2.0@mail.ru>
2023-08-11 17:38:13 +0300

    make medea adaptation

--- page.js
+++ page.js
@@ -1,47 +1,17 @@
-const linkS = document.querySelector("link");
-
-let i = 0;
-
-let widS = window.innerWidth;
-let heiS = window.innerHeight;
-let sizeS = widS > heiS;
-if (sizeS) {
-    linkS.href = "style.css";
-} else {
-    linkS.href = "styleRE.css";
-}
-
-let buttonR1 = document.getElementById("rateButton1");
-let buttonR2 = document.getElementById("rateButton2");
-let buttonD = document.getElementById("rateButtonD");
-let el1 = document.getElementById("disEl1");
-let el2 = document.getElementById("disEl2");
-
 function moreRates1() {
-  el1.classList.toggle("display");
-  buttonR1.classList.add("display");
-  buttonR2.classList.toggle("display");
+  disEl1.classList.toggle("display");
+  rateButton1.classList.add("display");
+  rateButton2.classList.toggle("display");
 }
 
 function moreRates2() {
-  el2.classList.toggle("display");
-  buttonR2.classList.add("display");
-  buttonD.classList.toggle("display");
+  disEl2.classList.toggle("display");
+  rateButton2.classList.add("display");
+  rateButtonD.classList.toggle("display");
 }
 
 function moreRatesD() {
-  buttonD.classList.add("display");
-  buttonR2.classList.add("display");
-  buttonR1.classList.toggle("display");
+  rateButtonD.classList.add("display");
+  rateButton2.classList.add("display");
+  rateButton1.classList.toggle("display");
 }
-
-window.addEventListener('scroll', function() {
-  let wid = window.innerWidth;
-  let hei = window.innerHeight;
-  let size = wid > hei;
-  if (size) {
-    linkS.href = "style.css";
-  } else {
-    linkS.href = "styleRE.css";
-}
-});

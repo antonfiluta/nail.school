e31b48bc6e7ee9ff5ed07575eeb2e4d1247ad213
antonfiluta <antonfiluta2.0@mail.ru>
2023-08-14 19:47:52 +0300

    feat: creaty slider in courses start

--- page.js
+++ page.js
@@ -1,17 +1,28 @@
 function moreRates1() {
-  disEl1.classList.toggle("display");
+  disEl1.classList.remove("display");
   rateButton1.classList.add("display");
-  rateButton2.classList.toggle("display");
-}
-
-function moreRates2() {
-  disEl2.classList.toggle("display");
-  rateButton2.classList.add("display");
-  rateButtonD.classList.toggle("display");
+  rateButtonD.classList.remove("display");
 }
 
 function moreRatesD() {
+  disEl1.classList.add("display");
   rateButtonD.classList.add("display");
-  rateButton2.classList.add("display");
-  rateButton1.classList.toggle("display");
+  rateButton1.classList.remove("display");
 }
+
+const prewB = document.getElementById('prew');
+const nextB = document.getElementById('next');
+let images = document.querySelector('.sliderImg')
+let width = images.offsetWidth;
+let position = 0;
+let offWidth = 73.6;
+
+function prew() {
+  position += 73.855;
+  marginBox.style.marginLeft = position + 'vw';
+};
+
+function next() {
+  position -= 73.855;
+  marginBox.style.marginLeft = position + 'vw';
+};
